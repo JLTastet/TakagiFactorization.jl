81bb76a606ecb03cbd4888ff554ee28563254ec2
Jean-Loup Tastet <jean-loup.tastet@alumni.epfl.ch>
2018-09-21 23:49:11 +0200

    Test with BigFloat.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,28 @@ using LinearAlgebra
 using Test
 
 @testset "Takagi" begin
-    A₁ = convert(Matrix{Complex{Float64}}, [1.0 2.0; 2.0 1.0])
-    d₁, U₁ = takagi_factor(A₁)
-    @test A₁ ≈ transpose(U₁) * d₁ * U₁
-    @test d₁ ≈ Diagonal([3.0, 1.0])
-    @test_broken U₁ ≈ [1 1; 1im -1im] / √2
-    A₂ = convert(Matrix{Complex{Float64}}, [0.0 1.0; 1.0 0.0])
-    d₂, U₂ = takagi_factor(A₂)
-    @test A₂ ≈ transpose(U₂) * d₂ * U₂
-    @test d₂ ≈ Diagonal([1.0, 1.0])
-    @test U₂ ≈ [1 1; -1im 1im] / √2
+    @testset "Examples" begin
+        A₁ = convert(Matrix{Complex{Float64}}, [1.0 2.0; 2.0 1.0])
+        d₁, U₁ = takagi_factor(A₁)
+        @test A₁ ≈ transpose(U₁) * d₁ * U₁
+        @test d₁ ≈ Diagonal([3.0, 1.0])
+        @test_broken U₁ ≈ [1 1; 1im -1im] / √2
+        A₂ = convert(Matrix{Complex{Float64}}, [0.0 1.0; 1.0 0.0])
+        d₂, U₂ = takagi_factor(A₂)
+        @test A₂ ≈ transpose(U₂) * d₂ * U₂
+        @test d₂ ≈ Diagonal([1.0, 1.0])
+        @test U₂ ≈ [1 1; -1im 1im] / √2
+    end
+    @testset "BigFloat" begin
+        A₁ = convert(Matrix{Complex{BigFloat}}, [1.0 2.0; 2.0 1.0])
+        d₁, U₁ = takagi_factor(A₁)
+        @test A₁ ≈ transpose(U₁) * d₁ * U₁
+        @test d₁ ≈ Diagonal([3.0, 1.0])
+        @test_broken U₁ ≈ [1 1; 1im -1im] / √big(2)
+        A₂ = convert(Matrix{Complex{BigFloat}}, [0.0 1.0; 1.0 0.0])
+        d₂, U₂ = takagi_factor(A₂)
+        @test A₂ ≈ transpose(U₂) * d₂ * U₂
+        @test d₂ ≈ Diagonal([1.0, 1.0])
+        @test U₂ ≈ [1 1; -1im 1im] / √big(2)
+    end
 end
